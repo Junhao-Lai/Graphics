4f8ffec29016bf40d513d89ad82130f4e49b76a1
Eric Lai <jlai38@ucsc.edu>
2024-01-12 16:10:13 -0800

    Getting started 16:00

--- Lab0/drag.js
+++ Lab0/drag.js
@@ -1,5 +1,20 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
-ctx.stroke();
+    
+ctx.fillStyle = '#F0F0F0';	
+ctx.fillRect(0, 0, c.width, c.height);
+
+    // Draw horizontal red axis
+    ctx.strokeStyle = "red";
+    ctx.beginPath();
+    ctx.moveTo(0, c.height / 2);
+    ctx.lineTo(c.width, c.height / 2);
+    ctx.stroke();
+
+    // Draw vertical green axis
+    ctx.strokeStyle = "green";
+    ctx.beginPath();
+    ctx.moveTo(c.width / 2, 0);
+    ctx.lineTo(c.width / 2, c.height);
+    ctx.stroke();
+    
