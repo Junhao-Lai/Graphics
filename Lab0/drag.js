dfac7450798d013f8926efbcfe77057285214e58
Eric Lai <jlai38@ucsc.edu>
2024-01-14 15:10:03 -0800

    WIP 15:21

--- Lab0/drag.js
+++ Lab0/drag.js
@@ -1,20 +1,53 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-    
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+// Set up the canvas
 ctx.fillStyle = '#F0F0F0';	
-ctx.fillRect(0, 0, c.width, c.height);
+ctx.fillRect(0, 0, 500, 500); //background fill
+ 
+// Draw horizontal x-axis red axis
+ctx.strokeStyle = "red";
+ctx.beginPath();
+ctx.moveTo(0, 250);
+ctx.lineTo(500, 250);
+ctx.stroke();
 
-    // Draw horizontal red axis
-    ctx.strokeStyle = "red";
-    ctx.beginPath();
-    ctx.moveTo(0, 250); //start 
-    ctx.lineTo(500,250); // dst 
-    ctx.stroke();
+// Draw vertical y-axis green axis
+ctx.strokeStyle = "green";
+ctx.beginPath();
+ctx.moveTo(250, 0);
+ctx.lineTo(250, 500);
+ctx.stroke();
 
-    // Draw vertical green axis
-    ctx.strokeStyle = "green";
+var isPressed = false;
+var startPoint = { x: 0, y: 0 };
+
+// Function to draw a line
+function drawLine(startPoint, endPoint) {
+    ctx.strokeStyle = "black";
     ctx.beginPath();
-    ctx.moveTo(250,0);
-    ctx.lineTo(250,500);
+    ctx.moveTo(startPoint.x, startPoint.y);
+    ctx.lineTo(endPoint.x, endPoint.y);
     ctx.stroke();
+}
+
+// Handle mouse clicks
+canvas.addEventListener("mousedown", function(event) {
+    if (event.button === 0) { // Left click
+        isPressed = true;
+        startPoint = { x: event.offsetX, y: event.offsetY };
+        console.log("Start Point:", startPoint);
+    }
+});
+
+// Handle mouse release
+canvas.addEventListener("mouseup", function(event) {
+    if (isPressed) {
+        isPressed = false;
+        var endPoint = { x: event.offsetX, y: event.offsetY };
+        console.log("End Point:", endPoint);
+        drawLine(startPoint, endPoint);
+    }
+});
+
 
