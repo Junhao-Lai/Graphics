dd5060ee9320f7a7a4aa213f013107ebbe29c4c5
Junhao-Lai <laix1449@yahoo.com>
2024-04-05 17:14:17 -0700

    17:05

--- asg0/asg0.js
+++ asg0/asg0.js
@@ -1,3 +1,6 @@
+var canvas = document.getElementById('A1');  
+var ctx = canvas.getContext('2d');
+
 function main() {  
   // Retrieve <canvas> element
   var canvas = document.getElementById('A1');  
@@ -7,28 +10,43 @@ function main() {
   } 
 
   // Get the rendering context for 2DCG
-  var ctx = canvas.getContext('2d');
   //var canvas = document.getElementById('A1');  
   //window.ctx = canvas.getContext("2d");
   // Draw a 黑 rectangle
   ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set color to black
   ctx.fillRect(0, 0, canvas.width, canvas.height);        // Fill a rectangle with the color
+
+
+  let v1 = {
+    elements: [2.25, 2.25] // Example vector components
+  };
+  drawVector(v1, "red");
+  
+
 }
 
-function handleDrawEvent(){
-  let v1 = document.getElementById("name").value;
-  console.log(v1);
+// function handleDrawEvent(){
+//   let v1 = document.getElementById("name").value;
+//   console.log(v1);
+//   var canvas = document.getElementById('A1');  
+//   var ctx = canvas.getContext('2d');
+//   ctx.strokeStyle = 'red'; 
+//   let cx = canvas.width/2;
+//   let cy = canvas.height/2;
+//   ctx.beginPath();
+//   ctx.moveTo(cx,cy);
+//   ctx.lineTo(cx + 75, cy + 50);
+//   ctx.stroke();
+// }
 
-  var canvas = document.getElementById('A1');  
-  var ctx = canvas.getContext('2d');
+function drawVector(v,color){
 
-  ctx.strokeStyle = 'red'; 
-
+  ctx.strokeStyle = color;
   let cx = canvas.width/2;
   let cy = canvas.height/2;
   ctx.beginPath();
   ctx.moveTo(cx,cy);
-  ctx.lineTo(cx + 75, cy + 50);
+  ctx.lineTo(cx+v.elements[0]*20, cy-v.elements[1]*20);
   ctx.stroke();
-
 }
+
