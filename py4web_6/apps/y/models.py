5d181dfee8996f0ee1fbc9d1acd98f8b252feec1
Eric Lai <jlai38@ucsc.edu>
2024-05-13 15:23:03 -0700

    changes as for 15:23

--- py4web_6/apps/y/models.py
+++ py4web_6/apps/y/models.py
@@ -4,12 +4,14 @@ This file defines the database models
 
 from .common import db, Field, auth
 from pydal.validators import *
+import datetime 
 
 db.define_table(
     "url_map",
     Field("long_url", requires=IS_NOT_EMPTY()),
     auth.signature,
-)
+    Field("post_date", "datetime", readable=False, writable=False, default=lambda:datetime.datetime.now()),
+    )
 
 if db(db.url_map).count() == 0:
     db.url_map.insert(
